05e2aca495da001e192fea5177eed824318a61ab
chenifargan <chenifargan12@gmail.com>
2022-05-18 14:06:22 +0300

    Initial Commit

---
+++ hw02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw02
+//
+//  Created by חן איפרגן on 18/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
